12c935bc1ef4cfe330feb7710a180101f8197774
Cesar Ramos <97855377+livingcesium@users.noreply.github.com>
2025-02-16 04:16:52 -0500

    Starting to implement core functionality. All of it is in API_Driver class. Can GET record by ID, and can POST XML queries.

---
+++ app/__init__.py
@@ -0,0 +1,171 @@
+import os
+import base64
+import hashlib
+from dataclasses import dataclass, field
+from enum import Enum
+from typing import Dict
+os.environ['default_proxy_port'] = "80"
+
+import requests
+from lxml import etree # Suggested to use lxml for XML parsing, other option is xml.etree.ElementTree
+
+default_proxy_port = 80
+
+# This is a dataclass that represents the information needed for a connection to the EIDR API
+@dataclass
+class EIDR_Config:
+    url: str = None
+    party: str = None
+    user: str = None
+    password: str = None
+    hash: str = None
+    multipart: bool = False
+    boundary: str = "_EIDR MULTIPART_"
+    pagesize: int = 1000
+    fake_trust_manager: bool = False
+    report_scores: bool = False
+    retry_count: int = 3
+    use_gzip: bool = False
+    proxy_host: str = None
+    proxy_port: int = None
+    proxy_user: str = None
+    proxy_password: str = None
+    headers: Dict[str, str] = field(default_factory=dict)
+
+    """
+    Create an instance of EIDR_Config from an XML string.
+    :param xml: XML string to parse.
+    :return: EIDR_Config instance.
+    """
+    @classmethod
+    def from_xml(cls, xml: str):
+        root = etree.fromstring(xml.encode('utf-8'))
+        options = {}
+
+        for child in list(root.find('EndpointInfo')):
+            options[child.tag] = child.text
+        print(options)
+        config = EIDR_Config(
+            url = options.get('BaseURL'),
+            party = options.get('Party'),
+            user = options.get('User'),
+            password = options.get('Passwd'),
+        )
+        return config
+
+    def set_header(self, header: str, value: str):
+        self.headers.update({header: value})
+
+    def remove_header(self, header: str):
+        self.headers.pop(header, None)
+
+    def __post_init__(self):
+        if not self.url.endswith("/"):
+            self.url += "/"
+
+        shadow = base64.b64encode(hashlib.sha256(self.password.encode('utf-8')).digest()).decode('utf8')
+        auth_str = '%s:%s:%s' % (self.user, self.party, shadow)
+        self.set_header('Authorization', 'Eidr {}'.format(auth_str))
+        self.set_header( 'Accept', 'text/xml')
+        if self.multipart:
+            self.set_header('Content-Type', 'multipart/form-data; boundary={}'.format(self.boundary))
+        else:
+            self.set_header('Content-Type', 'text/xml')
+        self.password = shadow
+
+        if self.use_gzip:
+            self.set_header('gzip', '')
+
+    # TODO: This is GPT'd, either remove or fix
+    def to_xml(self):
+        print("You've been GPT'd!")
+        root = etree.Element('EIDR_Config')
+        for key, value in self.__dict__.items():
+            if value:
+                etree.SubElement(root, key).text = str(value)
+        return etree.tostring(root, pretty_print=True)
+
+# We will need to create a class for each of the services we want to use
+# Below is an enum that maps from the service name to the service endpoint
+class Service(Enum):
+    QUERY = 'query'
+    MATCH = 'match' # TODO: do this
+    GRAPH = 'object/graph' # TODO: do this.
+        # http params: extendedFamily = False
+        # xml: FindAncestors, FindDescendants, GetDependents, GetSeriesAncestry, GetLightweightRelationships, GetRemotestAncestor, GetLeafDescendants, GetParent, GetChildren
+    SERVICE_QUERY = 'service/query' # TODO: do this
+        # xml: FindServices, FindServicesByName, or FindServicesFromCatalog
+    SERVICE_REGISTER = 'service/create' # TODO: do this
+        # xml: CreateService
+    SERVICE_MODIFY = 'service/modify' # TODO: do this
+        # xml: Service
+    PARTY_QUERY = 'party/query?type={}' # TODO: do this
+        # url params: type = [ID | full]
+        # xml: FindParties, FindPartiesByName, FindPartiesFromCatalog
+    PASSWORD = '/user/password/{}?password={}' # TODO: do this
+        # url params: userdoi, new password
+    STATUS = 'status' # TODO: do this. Known as Cancellation Service also
+        # xml: Refer to schema
+# TODO: GET endpoints needed:
+# .../party/resolve/?type=[doi | full]
+# .../user/resolve/{userdoi}?type=[doi | full]
+
+
+# This is the main class that initiates a connection to EIDR and makes HTTP requests
+class API_Driver:
+
+    def __init__(self, config: EIDR_Config):
+        self.config = config
+    def get_object(self, object_id):
+        print(self.config.headers)
+
+        req = self.config.url + 'object/' + object_id + '?type=Full&followAlias=true'
+        resp = requests.get(req, headers=self.config.headers)
+        print(resp.content)
+        return resp.content
+
+
+    #def post(self, thing: xml_obj):
+
+    def post_raw(self, xml: str, service: Service):
+        # multipart might be better off ignored here, assume it is always false for now
+        data = xml if not self.config.multipart else (
+            "{}\n{}\n{}\n{}\n{}".format(
+                self.config.boundary,
+                'Content-Disposition: form-data; name={}'.format(service.value),
+                'Content-Transfer-Encoding: binary',
+                xml,
+                self.config.boundary
+            ))
+        print(self.config.headers)
+        print(data)
+        resp = requests.post(
+            self.config.url + service.value + "/",
+            data=data,
+            headers={**self.config.headers}
+        )
+        if resp.status_code != 200:
+            print("Error: {}".format(resp.status_code))
+        else: print(resp.content)
+
+        return resp
+
+def test():
+    with open("config.xml", "r") as file:
+        config = EIDR_Config.from_xml(file.read())
+    driver = API_Driver(config)
+    res = driver.get_object("10.5240/0EF3-54F9-2642-0B49-6829-R")
+#test()
+
+def test_post():
+    xml = ""
+    with open("test_post.xml", "r") as file:
+        xml = file.read()
+    with open("config.xml", "r") as file:
+        config = EIDR_Config.from_xml(file.read())
+    driver = API_Driver(config)
+
+    driver.post_raw(xml, Service.QUERY)
+test_post()
+
+
