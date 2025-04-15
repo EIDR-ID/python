85a96f926268335fec19d09919225ebc88e51cfc
Cesar Ramos <97855377+livingcesium@users.noreply.github.com>
2025-04-14 21:45:17 -0400

    Added permissions get request
    Preparing for file input system

--- app/driver.py
+++ app/driver.py
@@ -134,6 +134,13 @@ class QueryMode(Enum):
     ID = "ID"
     FULL = "full"
 
+class ACL_Type(Enum):
+    MODIFY = "Modify"
+    DELETE = "Delete"
+    READ_ACL = "ReadACL"
+    WRITE_ACL = "WriteACL"
+    READ_PROV = "ReadProvenance"
+
 
 class API_Driver:
 
@@ -173,6 +180,18 @@ class API_Driver:
         if doi_mode.lower() not in ["doi", "full"]:
             raise ValueError("Resolution mode must be either 'doi' or 'full'")
         req = self.config.url + 'party/resolve/' + party_id + '?type=' + doi_mode
+        resp = requests.get(req, headers=self.config.headers)
+        return resp.content.decode('utf-8')
+
+    # https://registry1.eidr.org/EIDR/permissions/read/%7BassetID%7D?aclType={aclType
+    # returns: An instance of the
+        # eidr:AdminResponse or
+        # eidr:PartyIDList
+
+    def get_permissions(self, object_id: str, acl_type: str | ACL_Type):
+        if isinstance(acl_type, ACL_Type):
+            acl_type = acl_type.value
+        req = self.config.url + 'permissions/read/' + object_id + '?aclType=' + acl_type
         resp = requests.get(req, headers=self.config.headers)
         return resp.content.decode('utf-8')
 
@@ -275,5 +294,15 @@ def test_video_service_get():
     res = driver.get_video_service("10.5239/170B-1D36", ResolveMode.FULL)
     print(res)
 
+def test_permissions():
+    driver = API_Driver.from_default()
+    res = driver.get_permissions("10.5240/8B55-F9AA-007F-B18E-C000-6", ACL_Type.MODIFY)
+    print(res)
 
-test_video_service_get()
+if __name__ == "__main__":
+    # test_post_file()
+    # test_get()
+    # test_query()
+    # test_video_service_get()
+    # test_permissions()
+    pass
--- app/manager.py
+++ app/manager.py
@@ -6,16 +6,25 @@ from typing import List, Tuple, TypedDict
 from requests import Session
 from xsdata.formats.dataclass.parsers.config import ParserConfig
 
-from app.scheme.org.eidr.schema import RegistrantType
+from app.scheme.org.eidr.schema import RegistrantType, PartyDoilistType, PartyIdlist
 from app.services import RegistryRequest, ServiceBase, ResponseReader, Query, StatusRequest, Delete
-from app.driver import API_Driver, ResolveMode
+from app.driver import API_Driver, ResolveMode, ACL_Type
 from app.services.metadata import BaseObjectMeta, FullMeta, ServiceMeta, PartyMeta
 from app.services.party_query import PartyQuery
+from app.services.registration.modify import Modify
 from app.services.res import test_query, parser, config
 from app.services.service_query import NoOperationRequest, ServiceQuery
 from app.services.simple_metadata import SimpleMetadata
 from app.services.res import ResponseType
 from app.scheme.org.eidr.schema.base_object_info_type import BaseObjectInfoType
+
+
+class AdminResponseError(Exception):
+    ...
+
+def check_err(res: ResponseReader):
+    if res.admin_response:
+        raise AdminResponseError("\n\nAdmin Response Code {}: {}\n\t{}".format(*res.status, res.get_field("details")))
 
 
 class SessionManager:
@@ -48,8 +57,9 @@ class SessionManager:
             raise ValueError("Must call query with query request")
         res = self.post(q)
         if res.status[0] != 0:
-            print(res.obj)
+            #print(res.obj)
             raise RuntimeError("Got bad status {}".format(res.status))
+        check_err(res)
         q_res = res.get_field("query_results")
         matched = [SimpleMetadata(data, driver=self.driver) for data in q_res.simple_metadata]
         return matched, q_res.continuation_token
@@ -57,11 +67,14 @@ class SessionManager:
     def status(self, s: RegistryRequest) -> Tuple[List[dict], str]:
         if s.name != "status":
             raise ValueError("Must call status with status request")
-        print(s.xml)
+        #print(s.xml)
         res = self.post(s)
-        print(res.obj)
+        #print(res.obj)
+        if res.status[0] != 0:
+            raise RuntimeError("Got bad status {}".format(res.status))
+        check_err(res)
         status_res = res.get_field("request_status_results")
-        print(status_res)
+        #print(status_res)
         operation_stats = [{
             "token": op_res.token,
             "status": (op_res.status.code.value, op_res.status.type_value.value),
@@ -74,6 +87,9 @@ class SessionManager:
         if s.name != "status":
             raise ValueError("Must call status with status request")
         res = self.post(s)
+        if res.status[0] != 0:
+            raise RuntimeError("Got bad status {}".format(res.status))
+        check_err(res)
         status_res = res.get_field("request_status_results")
         operation_stats = [{
             "token": op_res.token,
@@ -94,7 +110,8 @@ class SessionManager:
         if s.name != "service/query":
             raise ValueError("Must call service query with service query request")
         res = self.post(s, res_type=ResponseType.SERVICE)
-        print(res)
+        check_err(res)
+        #print(res)
         q_res = res.obj
         if q_res.continuation_token is not None:
             self.tokens.append(q_res.continuation_token)
@@ -117,18 +134,38 @@ class SessionManager:
         res = self.driver.get_party(party_id, resolve_mode)
         return PartyMeta.from_string(res)
 
+
+    # TODO: check
     def party_query(self, p: NoOperationRequest, full: bool = True):
         if p.name != "party/query":
             raise ValueError("Must call party query with party query request")
         res: ResponseReader
         matches: List[PartyMeta | str] = []
+
         if full:
             res = self.post(p, res_type=ResponseType.PARTY, params={"type": "full"})
+            check_err(res)
             matches = [PartyMeta(p) for p in res.get_field("party")]
         else:
             res = self.post(p, res_type=ResponseType.PARTY, params={"type": "ID"})
+            check_err(res)
             matches = [PartyMeta(p) for p in res.get_field("party_id")]
         if res.continuation_token:
             self.tokens.append(res.continuation_token)
         return matches
 
+    def permissions(self, object_id: str, acl_type: str | ACL_Type = ACL_Type.MODIFY):
+        res = ResponseReader(self.driver.get_permissions(object_id, acl_type))
+        check_err(res)
+        if res.status[0] != 0:
+            raise RuntimeError("Got bad status {}".format(res.status))
+        out: List[str] = res.get_field("party_id")
+        return out
+
+def test_permissions():
+    ses = SessionManager.from_default()
+    res = ses.permissions("10.5240/8B55-F9AA-007F-B18E-C000-6", acl_type=ACL_Type.MODIFY)
+    print(res)
+
+if __name__ == "__main__":
+    test_permissions()
