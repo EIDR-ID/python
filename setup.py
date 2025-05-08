8a4b441314290b2a78ea37a9d466b9ecfe1ace02
Edersen Cadeau <edersenc@gmail.com>
2025-05-07 23:32:31 -0400

    updated setup.py to include requirements
    Signed-off-by: Edersen Cadeau <edersenc@gmail.com>

--- setup.py
+++ setup.py
@@ -1,6 +1,12 @@
 from setuptools import setup, find_packages
+
+# Read requirements.txt
+with open("requirements.txt") as f:
+    requirements = f.read().splitlines()
+
 setup(
     name='py_eidr',
     version='0.1',
-    packages=find_packages()
+    packages=find_packages(),
+    install_requires=requirements,
 )
